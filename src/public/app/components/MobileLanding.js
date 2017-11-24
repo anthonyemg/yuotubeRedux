bf7b5cc8fa268e1c6a4f21bdee9a68db48d4ea9c
Anthony Greenheck <aemgreenheck@gmail.com>
2017-11-24 12:53:53 -0500

    [added] prop types to MobileLanding component

--- src/public/app/components/MobileLanding.js
+++ src/public/app/components/MobileLanding.js
@@ -1,29 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  videos: PropTypes.array.isRequired,
+  handleSelectVideo: PropTypes.func.isRequired,
+  convertDate: PropTypes.func.isRequired
+};
 
 class MobileLanding extends React.Component {
   constructor(props) {
-    super(props)
+    super();
   }
   render() {
     return (
-      <div className='mobileLanding-container mobile'>
-        <div className='mobileLanding-wrapper'>
+      <div className="mobileLanding-container mobile">
+        <div className="mobileLanding-wrapper">
           {this.props.videos.map((video, idx) => (
-            <div className='mobileLanding-video' key={idx} onClick={() => this.props.handleSelectVideo(video)}>
-              <div className='mobileLanding-videoImage' >
+            <div
+              className="mobileLanding-video"
+              key={idx}
+              onClick={() => this.props.handleSelectVideo(video)}
+            >
+              <div className="mobileLanding-videoImage">
                 <img src={video.snippet.thumbnails.high.url} />
               </div>
-              <div className='landingVideoList-description'>
-                <span className='landingVideoList-descriptionTitle'>{video.snippet.title}</span>
-                <span className='landingVideoList-descriptionSubTitle'>{video.snippet.channelTitle}</span>
-                <span className='landingVideoList-descriptionSubTitle'>{this.props.convertDate(video.snippet.publishedAt)}</span>
+              <div className="landingVideoList-description">
+                <span className="landingVideoList-descriptionTitle">{video.snippet.title}</span>
+                <span className="landingVideoList-descriptionSubTitle">
+                  {video.snippet.channelTitle}
+                </span>
+                <span className="landingVideoList-descriptionSubTitle">
+                  {this.props.convertDate(video.snippet.publishedAt)}
+                </span>
               </div>
             </div>
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
+
+MobileLanding.propTypes = propTypes;
 
 export default MobileLanding;
